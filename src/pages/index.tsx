e4dbfbe3060e2cc7e1c6c7618685459dd27d14c8
Bryan Zapata <bmzc95@gmail.com>
2021-04-27 21:16:57 -0500

    connect with json server

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,21 @@
-import Header from "../components/Header";
 
-export default function Home() {
+interface HomeProps {
+  episodes: any;
+}
+
+export default function Home(props: HomeProps) {
   return (
    <h1>Index</h1>
   )
+}
+
+export async function getServerSideProps() {
+  const response = await fetch('http://localhost:3333/episodes');
+  const data = await response.json();
+
+  return {
+    props : {
+      episodes: data,
+    }
+  }
 }
